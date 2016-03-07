bbcf4ae483f3108d4c9211552b740ba9e2354e50
wburford1 <william_burford@Wills-MacBook-Air.local>
2016-03-06 21:14:34 -0500

    fish slows down for no good reason

--- FallingFish/GameScene.swift
+++ FallingFish/GameScene.swift
@@ -8,7 +8,17 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    
+    struct PhysicsCategory {
+        static let None      : UInt32 = 0
+        static let All       : UInt32 = UInt32.max
+        static let Fish   : UInt32 = 0b0       // 0
+        static let Wall   : UInt32 = 0b1       // 1
+        static let Death  : UInt32 = 0b10      // 2
+    }
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
@@ -22,8 +32,16 @@ class GameScene: SKScene {
         let fishWidth = size.width/15
         fish.size = CGSize(width: fishWidth, height: (fishWidth)/(500/139))
         fish.position = CGPoint(x: size.width/2, y: size.height*3/4)
-        let fishXMotion = SKAction.moveBy(CGVector(dx: -size.width/5, dy: 0), duration: 5)
-        fish.runAction(fishXMotion)
+        fish.physicsBody = SKPhysicsBody(rectangleOfSize: fish.size)
+        fish.physicsBody?.dynamic = true
+        fish.physicsBody?.collisionBitMask = 0b0
+        fish.physicsBody?.categoryBitMask = PhysicsCategory.Fish
+        fish.physicsBody?.contactTestBitMask = PhysicsCategory.Wall | PhysicsCategory.Death
+        fish.physicsBody?.restitution = 1.0
+        fish.physicsBody?.friction = 0;
+        fish.physicsBody?.affectedByGravity = false
+        fish.physicsBody?.usesPreciseCollisionDetection = true;
+        //fish.physicsBody?.velocity = (CGVector(dx: size.width/10, dy: 0))
         self.addChild(fish)
         
         //wall placement does not work correctly on iPads and iPhone 4s
@@ -32,13 +50,41 @@ class GameScene: SKScene {
         let leftWall = SKSpriteNode(imageNamed: "solidWall.png")
         leftWall.size = CGSize(width: size.width/20, height: size.height)
         leftWall.position = CGPoint(x: size.width/3-size.width/20, y: size.height/2)
+        leftWall.physicsBody = SKPhysicsBody(rectangleOfSize: leftWall.size)
+        leftWall.physicsBody?.categoryBitMask = PhysicsCategory.Wall
+        leftWall.physicsBody?.collisionBitMask = 0b1
+        leftWall.physicsBody?.contactTestBitMask = PhysicsCategory.Fish
+        leftWall.physicsBody?.affectedByGravity = false
+        leftWall.physicsBody?.dynamic = true
+        leftWall.physicsBody?.restitution = 1;
+        leftWall.physicsBody?.friction = 0;
         self.addChild(leftWall)
         
         let rightWall = SKSpriteNode(imageNamed: "solidWall.png")
         rightWall.size = CGSize(width: size.width/20, height: size.height)
         //rightWall.position = CGPoint(x: size.width+size.width/10.0), y: size.height/2)
         rightWall.position = CGPoint(x:size.width/1.4, y: size.height/2)
+        rightWall.physicsBody = SKPhysicsBody(rectangleOfSize: rightWall.size)
+        rightWall.physicsBody?.categoryBitMask = PhysicsCategory.Wall
+        rightWall.physicsBody?.contactTestBitMask = PhysicsCategory.Fish
+        rightWall.physicsBody?.collisionBitMask = 0b1
+        rightWall.physicsBody?.affectedByGravity = false
+        rightWall.physicsBody?.dynamic = true
+        rightWall.physicsBody?.restitution = 1;
+        rightWall.physicsBody?.friction = 0;
         self.addChild(rightWall)
+        
+        let sceneObjects = NSMutableArray()
+        sceneObjects.addObject(fish)
+        sceneObjects.addObject(leftWall)
+        sceneObjects.addObject(rightWall)
+        playInfinitGame(sceneObjects)
+    }
+    
+    func playInfinitGame(sceneObjects: NSMutableArray){
+        let fish = sceneObjects[0]
+        fish.physicsBody?!.velocity = (CGVector(dx: size.width, dy: 0))
+        print(fish.physicsBody)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -63,5 +109,34 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        print("didbegincontact")
+        // 1
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        
+        // 2
+        if ((firstBody.categoryBitMask & PhysicsCategory.Fish != 0) &&
+            (secondBody.categoryBitMask & PhysicsCategory.Wall != 0)) {
+                fishDidCollideWithWall(firstBody.node as! SKSpriteNode, wall: secondBody.node as! SKSpriteNode)
+        }
+        
+    }
+    
+    func fishDidCollideWithWall(fish: SKSpriteNode, wall: SKSpriteNode){
+        print("collision with wall")
+        let originalVelocity = fish.physicsBody?.velocity
+        fish.physicsBody?.velocity = CGVector(dx: (originalVelocity?.dx)!*(-1), dy: 0)
+        //fish.removeFromParent()
+        
     }
 }
